126c004cfebbf1fcc7cdbac7d95104f10267f543
Lewis <itslink957@gmail.com>
2021-09-06 10:03:45 -0400

    Added hello.py and operations.py files to Week4

---
+++ Week4/python_project_1/operations.py
@@ -0,0 +1,26 @@
+a = 10
+b = 3
+
+c = a + b
+print("Addition:", c)
+
+c = a - b
+print("Subtraction:", c)
+
+c = a * b
+print("Multiplication:", c)
+
+c = a / b
+print("Division:", c)
+
+c = a % b
+print("Modulo:", c)
+
+c = a // b
+print("Floor division:", c)
+
+c = a ** b
+print("Exponent:", c)
+
+c = -a
+print("Negative operation:", c)
