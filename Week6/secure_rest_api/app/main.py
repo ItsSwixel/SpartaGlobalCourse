ff3eafb530beb8d21a083c72945ea10b828f44e1
Lewis <itslink957@gmail.com>
2021-09-22 11:03:46 -0400

    Updated secure_rest_api script

--- Week6/secure_rest_api/app/main.py
+++ Week6/secure_rest_api/app/main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, make_response, request
+from random import random
 
 flask_app = Flask(__name__)
 
 
 @flask_app.route('/')
 def index_page():
-    return "This is the index page of a Secure REST API"
+    print(request.cookies)
+    if 'user_id' in request.cookies:
+        resp = make_response("Welcome back to the website!")
+    else:
+        user_id = random()
+        print(f"User ID: {user_id}")
+        resp = make_response("This is the index page of a Secure REST API")
+        resp.set_cookie('user_id', str(user_id))
+    return resp
 
 
 @flask_app.route('/help')
