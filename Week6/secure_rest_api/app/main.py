586937f636a0ce668b6aaa9b1e985a5ec55864c6
Lewis <itslink957@gmail.com>
2021-09-23 06:12:34 -0400

    updated secure_rest_api script

--- Week6/secure_rest_api/app/main.py
+++ Week6/secure_rest_api/app/main.py
@@ -1,13 +1,29 @@
 from flask import Flask, make_response, request, render_template
 from random import random
+import jwt
+import datetime
 
+SECRET_KEY = "E122B47F25FDA46567B5E88AC42F4"
 flask_app = Flask(__name__)
+
+
+def verify_token(token):
+    if token:
+        decoded_token = jwt.decode(token, SECRET_KEY, "HS256")
+        print(decoded_token)
+        # Check whether information in decoded_token is correct or not
+        return True  # If the information is correct, otherwise return False
+    else:
+        return False
 
 
 @flask_app.route('/')
 def index_page():
     print(request.cookies)
-    if 'user_id' in request.cookies:
+    isUserLoggedIn = False
+    if 'token' in request.cookies:
+        isUserLoggedIn = verify_token(request.cookies['token'])
+    if isUserLoggedIn:
         return "Welcome back to the website!"
     else:
         user_id = random()
@@ -27,14 +43,22 @@ def login_page():
     return render_template('login.html')
 
 
+def create_token(username, password):
+    validity = datetime.datetime.utcnow() + datetime.timedelta(days=15)
+    token = jwt.encode({'user_id': 197345, 'username': username, 'expiry': str(validity)}, SECRET_KEY, "HS256")
+    return token
+
+
 @flask_app.route('/authenticate', methods=["POST"])
 def authenticate_users():
     data = request.form
     username = data['username']
     password = data['password']
     # Check whether username and password are correct
+    user_token = create_token(username, password)
     resp = make_response("Logged in successfully")
-    resp.set_cookie("loggedIn", 'True')
+    # resp.set_cookie("loggedIn", 'True')
+    resp.set_cookie('token', user_token)
     return resp
 
 
